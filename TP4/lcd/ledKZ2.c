89ea205c220467b4af579c8161a453e9dd1d9e3e
Yasma KADI <yasmakadi@gmail.com>
2019-03-08 19:47:46 +0100

    TP4 init
    Reste a faire le driver et les masks a reporter sur le CR

---
+++ TP4/lcd/ledKZ2.c
@@ -0,0 +1,162 @@
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <asm/io.h>
+#include <mach/platform.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Charlie, 2015");
+MODULE_DESCRIPTION("Module, aussitot insere, aussitot efface");
+
+#define NBMAX_LED 32
+#define LED0 4
+#define LED1 17
+
+#define LCD_IOCCLEAR()
+
+static int major;
+static int leds[NBMAX_LED];
+static int nbled = 2;
+static int btn = 18;
+
+struct gpio_s
+{
+    uint32_t gpfsel[7];
+    uint32_t gpset[3];
+    uint32_t gpclr[3];
+    uint32_t gplev[3];
+    uint32_t gpeds[3];
+    uint32_t gpren[3];
+    uint32_t gpfen[3];
+    uint32_t gphen[3];
+    uint32_t gplen[3];
+    uint32_t gparen[3];
+    uint32_t gpafen[3];
+    uint32_t gppud[1];
+    uint32_t gppudclk[3];
+    uint32_t test[1];
+}
+volatile *gpio_regs = (struct gpio_s *)__io_address(GPIO_BASE);
+
+module_param_array(leds, int, &nbled, 0);
+MODULE_PARM_DESC(LEDS, "tableau des numéros de port LED");
+module_param(btn, int, 0);
+MODULE_PARM_DESC(btn, "numéro du port du bouton");
+
+static int count = 0;
+
+static void gpio_fsel(int pin, int fun)
+{
+    uint32_t reg = pin / 10;
+    uint32_t bit = (pin % 10) * 3;
+    uint32_t mask = 0b111 << bit;
+    gpio_regs->gpfsel[reg] = (gpio_regs->gpfsel[reg] & ~mask) | ((fun << bit) & mask);
+}
+
+static void gpio_write(int pin, bool val)
+{
+    if (val)
+        gpio_regs->gpset[pin / 32] = (1 << (pin % 32));
+    else
+        gpio_regs->gpclr[pin / 32] = (1 << (pin % 32));
+}
+
+/* Do no fill with '\n' */
+static void gpio_read(int pin, char *buf, size_t buff_size) {
+    printk(KERN_DEBUG "reglev[0] : %x %x\n", gpio_regs->gplev[btn / 32], ((gpio_regs->gplev[btn / 32])>>18)&1);
+    //if (buff_size) {
+      if (((gpio_regs->gplev[btn / 32] >>  btn)&1) == 0) {
+        buf[0] = '0';
+      } else {
+        buf[0] = '1';
+      }
+      printk(KERN_DEBUG "KY - read btn value = %c \n", buf[0]);
+    //}
+}
+
+
+static int 
+open_ledbp(struct inode *inode, struct file *file) {
+    count ++;
+    printk(KERN_DEBUG "KY - open() : cpt = %d\n", count);
+    return 0;
+}
+
+static ssize_t 
+read_ledbp(struct file *file, char *buf, size_t count, loff_t *ppos) {
+    gpio_read(btn, buf, count);
+    return count;
+}
+
+static ssize_t 
+write_ledbp(struct file *file, const char *buf, size_t count, loff_t *ppos) {
+    if (buf[0] == '0') 
+      gpio_write(LED0, 0);
+    else 
+      gpio_write(LED0, 1);
+    return count;
+}
+
+static int 
+release_ledbp(struct inode *inode, struct file *file) {
+    count --;
+    printk(KERN_DEBUG "KY - close() : cpt = %d\n", count);
+    return 0;
+}
+
+static int my_ioctl(struct inode *inodep,struct file *filep,unsigned int cmd, unsigned long arg) {
+    unsigned long err = 0;
+    unsigned long n,z,i;
+    struct lcd_data  *p = filep->private_data;
+    switch(cmd)
+    {
+      case  LCD_IOCCLEAR :
+      filep->f_pos = 0;
+      p->nbcar = sizeof(data);
+      for(i=0;i<sizeof(data);i++) p->mess[i] = data[i];
+      return 0;
+        case  LCD_IOCTCURPOS :
+      ...
+      return 0;
+        case  LCD_IOCSDATA :
+      ...
+      return 0;
+        default :
+      return -EINVAL;     /* Invalid argument   */
+    }
+}
+
+struct file_operations fops_ledbp =
+{
+    .open       = open_ledbp,
+    .read       = read_ledbp,
+    .write      = write_ledbp,
+    .ioctl      = my_ioctl,
+    .release    = release_ledbp 
+};
+
+static int __init mon_module_init(void)
+{
+   major = register_chrdev(0, "ledKZ2", &fops_ledbp);
+   printk(KERN_DEBUG "KY - Hello YAS and KAI!\n");
+   printk(KERN_DEBUG "KY - btn=%d !\n", btn);
+   printk(KERN_DEBUG "KY - nbled=%d !\n", nbled);
+   int i=0;
+   for (i=0; i < nbled; i++)
+       printk(KERN_DEBUG "LED %d = %d\n", i, leds[i]);
+   gpio_fsel(btn, 0);
+   gpio_fsel(LED0, 1);
+   gpio_fsel(LED1, 1);
+   return 0;
+}
+
+static void __exit mon_module_cleanup(void)
+{
+    unregister_chrdev(major, "ledKZ2");
+    printk(KERN_DEBUG "KY - Goodbye YAS and KAI!\n");
+}
+
+
+module_init(mon_module_init);
+module_exit(mon_module_cleanup);
+
---
+++ TP4/peri_tp4_Zhuang_Kadi.md
@@ -0,0 +1,124 @@
+# TP4: Pilotage d'un écran LCD en mode utilisateur et par un driver
+
+```
+Yasma KADI
+Kailun ZHUANG
+```
+
+### Configuration des GPIO pour le pilotage de l'écran
+
+- On peut configurer les GPIOs en utilisant la fonction `gpfsel()` qui permet d'écrire dans les registres GPFSELx (1: output, 0: input). Dans notre cas, on considère les signaux RS, E et Dx comme des sorties (pour la raspberry pi).
+
+- On peut utiliser la fonction `gpio_write` pour écrire des valeurs dans les registres de l'écran LCD.
+
+Pour initialiser\
+- On envoie respectivement: 
+	- 0011
+	- 0011
+	
+	- 0011
+	- 0010
+
+Fonction set
+	- 0010
+	- 1000
+
+Display control
+	- 0000
+	- 1100
+
+Entry mode set
+	- 0000
+	- 0100
+
+Display clear
+	- 0000
+	- 0001
+
+Affichage d'un caractère :\
+On met le cursor : LCD_SETDDRAMADDR + offset, \
+Puis envoyer l'octet avec `lcd_write4bits`.
+
+Pour lancer une commande : \
+On écrit 0 dans `RS`, envoyer l'octet de commande correspondant.
+
+### Fonctionnement de l'écran et fonctions de base
+
+
+##### Q1
+Le mot clé `volatile` sert à indiquer au compilateur de ne pas optimiser les accès à la variable avec laquelle il est utilisé.
+
+##### Q2
+O_RDWR : ouvrire en lecture et écriture \
+O_SYNC : bloquant jusqu'à la terminaison des accès mémoire\
+PROT_READ | PROT_WRITE : donner la permission lecture et écriture\
+MAP_SHARED : partage la mémoire avec les autres processus.
+
+##### Q3
+Il faudrait bien libérer les ressources allouées avec mmap.
+
+##### Q4
+//TODO
+
+LCD_FUNCTIONSET         0b00100000
+LCD_FS_2LINE            0b00001000
+LCD_FS_4BITMODE         0b00000000
+LCD_FS_5x8DOTS          0b00000000
+						=0010 
+						 1000
+
+LCD_DISPLAYCONTROL      0b00001000
+LCD_DC_DISPLAYON        0b00000100
+LCD_DC_CURSOROFF        0b00000000
+   						=0000 
+   						 1100
+
+LCD_ENTRYMODESET        0b00000100
+LCD_EM_RIGHT            0b00000000
+LCD_EM_DISPLAYNOSHIFT   0b00000000
+						=0000 
+						 0100
+
+
+LCD_CLEARDISPLAY        0b00000001
+						=0000
+						 0001
+LCD_RETURNHOME          0b00000010
+ 						=0000
+ 						 0010
+
+##### Q5
+La fonction `lcd_message` remplit un tableau de taille 4 x 20.\
+Elle écrit sur la ligne suivante si elle arrive au bout de la ligne, dans la limite de 80 caractères.
+
+2. Positionnement du curseur
+
+```c
+void lcd_set_cursor(int x, int y)
+{   
+	// condition invalide
+    if (x > 19 || y > 3 || x < 0 || y < 0) {
+        printf("bad cursor\n");
+        return;
+    }
+    int a[] = { 0, 0x40, 0x14, 0x54 };
+    lcd_command(LCD_SETDDRAMADDR + a[y] + x);
+}
+
+```
+
+3. Affichage des paramètres sur le LCD
+
+```c
+int i, k;
+for (i = 1; i < argc; ++i)
+{
+    lcd_set_cursor(5+i, i%4);
+    for (k = 0; k < strlen(argv[i][k]); ++k)
+    {
+        lcd_data(argv[i][k]);
+    }
+}
+```
+
+### Driver pour le LCD
